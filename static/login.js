d49364d35ffe615e4f90f3b7dd510ba8126ae3fb
BrandonCrowther <bcrowthe@unb.ca>
2016-11-27 18:02:53 -0400

    Working on logins. Untested ajax call added

--- static/login.js
+++ static/login.js
@@ -1,3 +1,23 @@
 $(function(){
 	console.log("China");
 });
+
+
+function login(){
+	var name = $('#username').val();
+	var password = $('#password').val();
+
+	$.ajax({
+		url: "/login",
+		data: {username: name, password: password},
+		dataType: "json",
+		type: "POST",
+		success: function(){
+			window.alert("You are successfully logged in as " + name);
+		},
+		failure: function(data){
+			window.alert("Something went wrong logging in");
+			console.log(data);
+		}
+	});
+}
